8e41836890ed2e276f9ebc359f9f30582223e878
Michal Bartosik <msbartosik@gmail.com>
2020-09-19 21:48:17 +0200

    adding other files, templates

---
+++ src/main/java/com/codecool/queststore/QueststoreApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.queststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueststoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueststoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/queststore/QueststoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.queststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueststoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
