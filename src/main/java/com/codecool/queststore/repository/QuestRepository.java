b92e7fe7d3365e1e099c31d5778a81f4d69f859c
irrehaare <wjtkwilk@gmail.com>
2020-10-01 21:35:27 +0200

    Implement loading quest data from database

--- src/main/java/com/codecool/queststore/repository/QuestRepository.java
+++ src/main/java/com/codecool/queststore/repository/QuestRepository.java
@@ -1,2 +1,9 @@
-package com.codecool.queststore.repository;public class QuestRepository {
+package com.codecool.queststore.repository;
+
+import com.codecool.queststore.model.Quest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestRepository extends JpaRepository<Quest, Long> {
 }
