462198123b816faa5fed81bef6b9f68c52cccc90
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-10-01 03:39:02 +0200

    working authentication

--- src/main/java/com/codecool/queststore/controller/UserController.java
+++ src/main/java/com/codecool/queststore/controller/UserController.java
@@ -10,9 +10,29 @@ public class UserController {
 //    String showLoginForm(){
 //        return "index";
 //    }
+//
+//    @GetMapping("/profile_page")
+//    String showProfile() {
+//        return "profile_page";
+//    }
 
-    @GetMapping("/profile_page")
-    String showProfile() {
-        return "profile_page";
+    @GetMapping("/")
+    public String root() {
+        return "index";
     }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/profile_page";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/profile_page";
+    }
+
+//    @GetMapping("/access-denied")
+//    public String accessDenied() {
+//        return "/error/access-denied";
+//    }
 }
