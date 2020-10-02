4afcadfaee55cbc869b23ff113e59b35f7ffc164
irrehaare <wjtkwilk@gmail.com>
2020-10-02 19:57:15 +0200

    Add changes from the meeting

--- src/main/resources/static/js/animate-quest-type.js
+++ src/main/resources/static/js/animate-quest-type.js
@@ -1,10 +1,21 @@
 function selectBasicQuests() {
     document.getElementById("type-choice").style.background="linear-gradient(90deg, #001513 5%, #550609 13%, #550609 40%, #001513 47%)"
+    changeClassDisplay('extra', 'none')
+    changeClassDisplay('basic', 'flex')
 }
 
 
 function selectExtraQuests() {
     document.getElementById("type-choice").style.background="linear-gradient(90deg, #001513 53%, #550609 60%, #550609 87%, #001513 95%)"
+    changeClassDisplay('extra', 'flex')
+    changeClassDisplay('basic', 'basic')
+}
+
+function changeClassDisplay(elementClass, display) {
+    let elements = document.getElementsByClassName(elementClass);
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].style.display = display;
+    }
 }
 
 document.getElementById("basic-quests").addEventListener("click", selectBasicQuests);
