8f9621be49dcfac3a28fc6a7e81bd75935f82a8f
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-10-22 18:53:40 +0200

    fixed tests configuration

--- src/test/java/com/codecool/queststore/repository/UserRepositoryTest.java
+++ src/test/java/com/codecool/queststore/repository/UserRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.codecool.queststore.repository;
 
 import com.codecool.queststore.model.User;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -10,9 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
 @Sql("testUsers.sql")
 @DataJpaTest
 public class UserRepositoryTest {
@@ -21,6 +23,7 @@ public class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
+    @DisplayName("should return true")
     public void should_find_by_username() {
         //given
         String username = "username";
--- src/test/java/com/codecool/queststore/service/StudentItemServiceTest.java
+++ src/test/java/com/codecool/queststore/service/StudentItemServiceTest.java
@@ -1,15 +1,16 @@
 package com.codecool.queststore.service;
 
 import com.codecool.queststore.repository.StudentItemRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class StudentItemServiceTest {
     @Mock
     private StudentItemRepository studentItemRepository;
