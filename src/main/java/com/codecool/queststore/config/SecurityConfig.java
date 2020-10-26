7d46a84cfb0fa095abea8566d5e5a5b992cb4b60
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-10-26 08:57:24 +0100

    security fix

--- src/main/java/com/codecool/queststore/config/SecurityConfig.java
+++ src/main/java/com/codecool/queststore/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             ).hasAnyRole("ADMIN", "STUDENT", "MENTOR")
                     .antMatchers(
                             "/admin",
-                            "users/**"
+                            "/users/**"
                             ).hasRole("ADMIN")
                     .antMatchers(
                             "/mentor"
