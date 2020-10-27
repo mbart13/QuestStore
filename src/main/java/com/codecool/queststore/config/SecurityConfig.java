25afac3282f12198c4bcba4b63fb5dacf6d1e227
Michal Bartosik <msbartosik@gmail.com>
2020-10-27 21:10:14 +0100

    fixed mess in login page

--- src/main/java/com/codecool/queststore/config/SecurityConfig.java
+++ src/main/java/com/codecool/queststore/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -70,17 +69,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-
         auth.userDetailsService(userDetailsService);
     }
 
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
         return new BCryptPasswordEncoder(10);
     }
-
 }
 
