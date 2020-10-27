1253f562c6ad919082612843373963bb4d02c17a
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-10-27 09:25:41 +0100

    refactor on registration

--- src/main/java/com/codecool/queststore/config/UserDetailsServiceImpl.java
+++ src/main/java/com/codecool/queststore/config/UserDetailsServiceImpl.java
@@ -18,7 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         Optional<User> user = userRepository.findByUsername(username);
         if(user.isEmpty()) {
             throw new UsernameNotFoundException("User not found");
