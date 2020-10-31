e009ae7e91aa6ac59080057163a4e87a0cad5d77
Michal Bartosik <msbartosik@gmail.com>
2020-10-31 19:05:17 +0100

    adding @notblank validation to password

--- src/main/java/com/codecool/queststore/dto/PasswordDto.java
+++ src/main/java/com/codecool/queststore/dto/PasswordDto.java
@@ -5,6 +5,7 @@ import com.codecool.queststore.validators.PasswordValid;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -15,9 +16,11 @@ public class PasswordDto {
     @PasswordValid
     private String oldPassword;
 
+    @NotBlank(message = "Fields cannot be blank")
     @Size(min=8, max=16, message = "Password must be between 8-16 characters")
     private String newPassword;
-    
+
+    @NotBlank(message = "Fields cannot be blank")
     @Size(min=8, max=16, message = "Password must be between 8-16 characters")
     private String matchingPassword;
 
