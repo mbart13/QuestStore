ff6bfb9bf842fb7498a7a5a8a4b3c17c382a7416
Michal Bartosik <msbartosik@gmail.com>
2020-10-31 14:27:30 +0100

    remove not needed file

--- src/main/resources/static/js/delete-message.js
+++
@@ -1,7 +0,0 @@
-const a = document.querySelector('#delete');
-const message = document.querySelector('#confirmation');
-
-a.addEventListener("click", (e) => {
-    e.preventDefault();
-    message.style.display = 'block';
-})
